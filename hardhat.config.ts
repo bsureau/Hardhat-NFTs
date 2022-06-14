871312e197e899547945201d8273f35445878a5a
bsureau <sureau.benjamin@gmail.com>
2022-06-14 10:37:03 +0000

    refactor: convert hardhat.config from js to ts

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,19 +1,13 @@
-import "hardhat-gas-reporter"
-import "@nomiclabs/hardhat-etherscan"
-import '@nomiclabs/hardhat-ethers'
+import "@typechain/hardhat"
 import "@nomiclabs/hardhat-waffle"
-import dotenv from "dotenv"
+import "@nomiclabs/hardhat-etherscan"
+import "@nomiclabs/hardhat-ethers"
+import "hardhat-gas-reporter"
+import "dotenv/config"
 import "solidity-coverage"
 import "hardhat-deploy"
-import '@typechain/hardhat'
-
-// You need to export an object to set up your config
-// Go to https://hardhat.org/config/ to learn more
-/**
- * @type import('hardhat/config').HardhatUserConfig
- */
-
-dotenv.config()
+import "solidity-coverage"
+import { HardhatUserConfig } from "hardhat/config"
 
 const COINMARKETCAP_API_KEY = process.env.COINMARKETCAP_API_KEY || ""
 const RINKEBY_RPC_URL =
@@ -21,7 +15,7 @@ const RINKEBY_RPC_URL =
 const PRIVATE_KEY = process.env.PRIVATE_KEY || ""
 const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY || ""
 
-module.exports = {
+const config: HardhatUserConfig = {
     defaultNetwork: "hardhat",
     networks: {
         hardhat: {
@@ -32,7 +26,6 @@ module.exports = {
             url: RINKEBY_RPC_URL,
             accounts: [PRIVATE_KEY],
             chainId: 4,
-            blockConfirmations: 6,
         },
     },
     solidity: {
@@ -65,3 +58,5 @@ module.exports = {
         timeout: 200000, // 200 seconds max for running tests
     },
 }
+
+export default config
