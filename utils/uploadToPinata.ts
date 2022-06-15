a3951d80afb0cd097f50c1e8062558987966ff80
bsureau <sureau.benjamin@gmail.com>
2022-06-15 17:54:12 +0000

    feat: upload random nft token metadatas on IPFS using Pinata

--- utils/uploadToPinata.ts
+++ utils/uploadToPinata.ts
@@ -2,6 +2,17 @@ import pinataSDK, { PinataPinResponse } from "@pinata/sdk"
 import path from "path"
 import fs from "fs"
 
+interface TokenUriMetadataAttributes {
+    trait_type: string, 
+    value: number
+}
+export interface TokenUriMetadata {
+    name: string,
+    description: string,
+    image: string,
+    attributes: TokenUriMetadataAttributes[]
+}
+
 const pinata = pinataSDK(process.env.PINATA_API_KEY || '', process.env.PINATA_API_SECRET || '');
 
 export async function storeImages(imagesFilePath: string) {
@@ -12,7 +23,7 @@ export async function storeImages(imagesFilePath: string) {
     let responses: PinataPinResponse[] = []
     
     console.log("Uploading to IPFS!")
-    
+
     for (const fileIndex in files) {
         const readableStreamForFile: fs.ReadStream = fs.createReadStream(`${fullImagesPath}/${files[fileIndex]}`)
 
@@ -27,3 +38,13 @@ export async function storeImages(imagesFilePath: string) {
     return { responses, files }
 }
  
+
+export async function storeTokeUriMetadata(metadata: TokenUriMetadata) {
+    try {
+        const response = await pinata.pinJSONToIPFS(metadata)
+        return response
+    } catch (error) {
+        console.log(error)
+    }
+    return null 
+}
